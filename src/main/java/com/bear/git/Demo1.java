e338679c5b9a67f27a3af97db0cc3dd0f4a984ed
bear <3081620871@qq.com>
2022-09-17 11:27:01 +0800

    master-1

--- src/main/java/com/bear/git/Demo1.java
+++ src/main/java/com/bear/git/Demo1.java
@@ -10,5 +10,6 @@ public class Demo1 {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2!");
+        System.out.println("hello master-1!");
     }
 }
