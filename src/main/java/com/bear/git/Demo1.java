2b89e54366b2a6195ecbe8fcb307a3c835d88784
bear <3081620871@qq.com>
2022-09-17 11:01:57 +0800

    second

---
+++ src/main/java/com/bear/git/Demo1.java
@@ -0,0 +1,13 @@
+package com.bear.git;
+
+/**
+ * @className: Demo1
+ * @author: 一把Po吉他
+ * @date: 2022/9/17 10:57
+ * @version: 1.0
+ **/
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
