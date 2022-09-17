39798d3ce4aed6fea1494d0cf15e0c0135da2f74
bear <3081620871@qq.com>
2022-09-17 15:04:33 +0800

    demo2

---
+++ src/main/java/com/bear/git/Demo2.java
@@ -0,0 +1,13 @@
+package com.bear.git;
+
+/**
+ * @className: Demo2
+ * @author: 一把Po吉他
+ * @date: 2022/9/17 14:49
+ * @version: 1.0
+ **/
+public class Demo2 {
+    public static void main(String[] args) {
+        System.out.println("哈哈");
+    }
+}
